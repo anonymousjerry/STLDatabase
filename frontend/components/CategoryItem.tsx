47fb31f130f0ef0381bb88207bc7c3c4583c1f6a
anonymousjerry <christopherthomasson421@gmail.com>
2025-07-18 00:07:36 -0700

    fix(frontend) categoryMenuList part

--- frontend/components/CategoryItem.tsx
+++ frontend/components/CategoryItem.tsx
@@ -20,10 +20,11 @@ interface CategoryItemProps {
 const CategoryItem = ({ title, children, href }: CategoryItemProps) => {
   return (
     <Link href={href}>
-      <div className="flex flex-col items-center aspect-square w-full justify-center cursor-pointer border-2  transition bg-white py-5 text-black hover:rounded-2xl  hover:border-[#b6e400] hover:shadow-[0_0_16px_0_rgba(0,0,0,0.32)]">
-        {children}
-
-        <div className="font-semibold text-xl">{title}</div>
+      <div className="flex flex-col items-center aspect-square w-full h-[200px] justify-center cursor-pointer border-2  transition bg-white pt-2 text-black hover:rounded-2xl  hover:border-[#b6e400] hover:shadow-[0_0_16px_0_rgba(0,0,0,0.32)]">
+        <div className="flex h-3/4">
+          {children}
+        </div>
+        <div className="flex font-semibold text-xl h-1/4 items-center">{title}</div>
       </div>
     </Link>
   );
